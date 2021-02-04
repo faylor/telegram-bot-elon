451f01039f8b7386f4e48bac962c4cf03d0d8132
faylor <32422243+faylor@users.noreply.github.com>
2021-02-04 18:25:22 +0000

    Add original content

--- bot/bot.py
+++ bot/bot.py
@@ -13,6 +13,61 @@ dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
 
 
+def getUrl():
+    #obtain a json object with image details
+    #extract image url from the json object
+    contents = requests.get('https://api.thecatapi.com/v1/images/search')
+    js = contents.json()
+    print(js)
+    url = js[0]["url"]
+    return url
+
+@dp.message_handler(text=['doge', 'cat'])
+def sendImage(update, context):
+    url = getUrl()
+    chat_id = update.message.chat_id
+    context.bot.send_photo(chat_id=chat_id, photo=url)
+
+@dp.message_handler(text=['me', 'you'])
+def sendTable(update, context):
+    name = update.effective_user.first_name
+    if "josh" in name.lower():
+        update.message.reply_text(f'Hello {update.effective_user.first_name}, you are Jelly Hands')
+    else:
+        update.message.reply_text(f'Hello {update.effective_user.first_name}, you are HODLing strong')
+
+@dp.message_handler(text=['prices', 'btc', 'lambo', 'whenlambo', 'price', '$'])
+def prices(update, context):
+    chat_id = update.message.chat_id
+    mains = ["BTC", "ETH", "GRT", "LTC", "ADA", "AAVE", "DOGE"]
+    out = ""
+    totes = 0
+    for l in mains:
+        p, c = get_price(l)
+        totes = totes + c
+        context.bot.send_message(chat_id=chat_id, text=f"{l} ${round(p,4)} {round(c,1)}% 1 hour")
+    
+    if totes<0:
+        context.bot.send_message(chat_id=chat_id, text="OUCH, NO LAMBO FOR YOU!")
+    elif totes>15:
+        context.bot.send_message(chat_id=chat_id, text="OK OK, LAMBO FOR YOU!")
+    else:
+        context.bot.send_message(chat_id=chat_id, text="MEH, MAYBE LAMBO. HODL.")
+
+def get_price(label):
+    price, change_1hr = 0, 0
+    logging.error("DOWNLOADING " + label)    
+    try:
+        url = "https://data.messari.io/api/v1/assets/" + label + "/metrics"
+        resp = requests.get(url)
+        js = resp.json()
+        price = js["data"]["market_data"]["price_usd"]
+        change_1hr = js["data"]["market_data"]["percent_change_usd_last_1_hour"]
+    except Exception as e:
+        logging.error(e)
+    return price, change_1hr
+
+
 @dp.message_handler()
 async def echo(message: types.Message):
     logging.warning(f'Recieved a message from {message.from_user}')
