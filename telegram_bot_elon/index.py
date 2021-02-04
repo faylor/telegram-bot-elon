9da6ef674908556626069023d747f4081b0d4fdf
faylor <32422243+faylor@users.noreply.github.com>
2021-02-04 17:15:43 +0000

    test webhook setup

--- telegram_bot_elon/index.py
+++ telegram_bot_elon/index.py
@@ -1,96 +1,66 @@
-import asyncio
-import json
-import requests
-import os
-from aiogram import Bot, Dispatcher, types
+import logging
 
-# TOKEN var for elon_bot
-TOKEN = os.environ["TELEGRAM_BOT"]
-
-# def getUrl():
-#     #obtain a json object with image details
-#     #extract image url from the json object
-#     contents = requests.get('https://api.thecatapi.com/v1/images/search')
-#     js = contents.json()
-#     print(js)
-#     url = js[0]["url"]
-#     return url
-
-# def sendImage(update, context):
-#     url = getUrl()
-#     chat_id = update.message.chat_id
-#     context.bot.send_photo(chat_id=chat_id, photo=url)
-
-# def sendTable(update, context):
-#     name = update.effective_user.first_name
-#     if "josh" in name.lower():
-#         update.message.reply_text(f'Hello {update.effective_user.first_name}, you are Jelly Hands')
-#     else:
-#         update.message.reply_text(f'Hello {update.effective_user.first_name}, you are HODLing strong')
-
-# def prices(update, context):
-#     chat_id = update.message.chat_id
-#     mains = ["BTC", "ETH", "GRT", "LTC", "ADA", "AAVE", "DOGE"]
-#     out = ""
-#     totes = 0
-#     for l in mains:
-#         p, c = get_price(l)
-#         totes = totes + c
-#         context.bot.send_message(chat_id=chat_id, text=f"{l} ${round(p,4)} {round(c,1)}% 1 hour")
-    
-#     if totes<0:
-#         context.bot.send_message(chat_id=chat_id, text="OUCH, NO LAMBO FOR YOU!")
-#     elif totes>15:
-#         context.bot.send_message(chat_id=chat_id, text="OK OK, LAMBO FOR YOU!")
-#     else:
-#         context.bot.send_message(chat_id=chat_id, text="MEH, MAYBE LAMBO. HODL.")
-
-# def get_price(label):
-#     price, change_1hr = 0, 0
-#     logging.error("DOWNLOADING " + label)    
-#     try:
-#         url = "https://data.messari.io/api/v1/assets/" + label + "/metrics"
-#         resp = requests.get(url)
-#         js = resp.json()
-#         price = js["data"]["market_data"]["price_usd"]
-#         change_1hr = js["data"]["market_data"]["percent_change_usd_last_1_hour"]
-#     except Exception as e:
-#         logging.error(e)
-#     return price, change_1hr
-
-# def error_callback(bot, error):
-#     logging.warning(str(error))
+from aiogram import Bot, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher.webhook import SendMessage
+from aiogram.utils.executor import start_webhook
 
 
-# updater = Updater(TOKEN)
-# #call sendImage() when the user types a command in the telegram chat
-# updater.dispatcher.add_handler(CommandHandler('doge',sendImage))
-# updater.dispatcher.add_handler(CommandHandler('me',sendTable))
-# updater.dispatcher.add_handler(CommandHandler('lambo',prices))
-# updater.dispatcher.add_handler(CommandHandler('greendildos',prices))
+API_TOKEN = os.environ["TELEGRAM_BOT"]
 
-# # Register it to the updater's dispatcher
-# updater.dispatcher.add_error_handler(error_callback)
+# webhook settings
+WEBHOOK_HOST = 'https://telegram-bot-elon.herokuapp.com/'
+WEBHOOK_PATH = ''
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
-# #start the bot
-# updater.start_polling()
-# updater.idle()
+# webserver settings
+WEBAPP_HOST = 'localhost'  # or ip
+WEBAPP_PORT = 8080
 
-async def start_handler(event: types.Message):
-    await event.answer(
-        f"Hello, {event.from_user.get_mention(as_html=True)} 👋!",
-        parse_mode=types.ParseMode.HTML,
-    )
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
 
 
-async def main():
-    bot = Bot(token=TOKEN)
-    try:
-        disp = Dispatcher(bot=bot)
-        disp.register_message_handler(start_handler, commands={"cat", "dog"})
-        await disp.start_polling()
-    finally:
-        await bot.close()
+@dp.message_handler()
+async def echo(message: types.Message):
+    # Regular request
+    # await bot.send_message(message.chat.id, message.text)
+
+    # or reply INTO webhook
+    return SendMessage(message.chat.id, message.text)
+
+
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+    # insert code here to run it after start
+
+
+async def on_shutdown(dp):
+    logging.warning('Shutting down..')
+
+    # insert code here to run it before shutdown
+
+    # Remove webhook (not acceptable in some cases)
+    await bot.delete_webhook()
+
+    # Close DB connection (if used)
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+
+    logging.warning('Bye!')
+
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
