d7625ffa73fb53bd1b5734c8de10736b3f60736f
faylor <32422243+faylor@users.noreply.github.com>
2021-02-05 17:22:30 +0000

    big changes

--- bot/bot.py
+++ bot/bot.py
@@ -57,6 +57,10 @@ async def send_help(message: types.Message):
 async def prices(message: types.Message):
     chat_id = message.chat.id
     mains = ["BTC", "ETH", "GRT", "LTC", "ADA", "AAVE", "DOGE", "ZIL"]
+    config = json.loads(r.execute_command('JSON.GET', message.chat.id))
+    logging.info(json.dumps(config))
+    if "watch_list" in config:
+        mains = config["watch_list"]
     out = "<pre>| Symbol|  Price      | +/- 1hr  |\n"
     totes = 0
     for l in mains:
@@ -101,13 +105,14 @@ def get_price(label):
         return 0, 0, 0
     return price, change_1hr, change_24hr
 
-@dp.message_handler(commands=['bets delete', 'weekly delete', 'weeklybets delete', '#weeklybets delete'])
-async def finish_weekly(message: types.Message):
+@dp.message_handler(commands=['bets start', 'weekly start', 'weeklybets start', '#weeklybets start'])
+async def start_weekly(message: types.Message):
     for key in r.scan_iter("BTC_*"):
         r.delete(key)
     for key in r.scan_iter("ETH_*"):
         r.delete(key)
     await bot.send_message(chat_id=message.chat.id, text="DELETED BETS. Good Luck.")
+
 
 def get_abs_difference(s, p):
     estimate = -999999
@@ -124,8 +129,7 @@ def get_abs_difference(s, p):
         logging.warn("Cannot convert abs difference:" + str(e))
         return -999999
 
-@dp.message_handler(commands=['bets', 'weekly', 'weeklybets', '#weeklybets'])
-async def get_weekly(message: types.Message):
+def weekly_tally(message: types.Message):
     p_btc, _, _ = get_price("btc")
     p_eth, _, _ = get_price("eth")
     amount=r.get("BTC_*") or 'Not Sure'
@@ -145,7 +149,7 @@ async def get_weekly(message: types.Message):
         out = out + name + " => " + a + "  -- DIFF = " + str(round(d,1)) + "\n"
     out = out + "\n LOOK WHO IS WINNING BTC == " + winning + "\n"
     out = out + "\nETH Bets (Current=" + str(round(p_eth,0)) + "):\n"
-    winning = ""
+    winning_eth = ""
     winning_diff = 99999
     for key in r.scan_iter("ETH_*"):
         a = r.get(key).decode('utf-8') or "NONE"
@@ -153,13 +157,42 @@ async def get_weekly(message: types.Message):
         name = str(key.decode('utf-8')).replace("ETH_","")
         if d <= winning_diff:
             if d == winning_diff:
-                winning = winning + ", " + name
+                winning_eth = winning + ", " + name
             else:
-                winning = name
+                winning_eth = name
                 winning_diff = d
         out = out + name + " => " + a + "  -- DIFF = " + str(round(d,1)) + "\n"
-    out = out + "\n LOOK WHO IS WINNING ETH == " + winning + "\n"
+    out = out + "\n LOOK WHO IS WINNING ETH == " + winning_eth + "\n"
+    return out, winning, winning_eth
+
+@dp.message_handler(commands=['bets', 'weekly', 'weeklybets', '#weeklybets'])
+async def get_weekly(message: types.Message):
+    out, _ = weekly_tally(message)
     await bot.send_message(chat_id=message.chat.id, text=out)
+
+@dp.message_handler(commands=['bets stop', 'weekly stop', 'weeklybets stop', '#weeklybets start'])
+async def finish_weekly(message: types.Message):
+    out, winning_btc, winning_eth = weekly_tally(message)
+    await bot.send_message(chat_id=message.chat.id, text=out)
+    await bot.send_message(chat_id=message.chat.id, text=f'BTC winner = {winning_btc}, ETH winner = {winning_eth}')
+    logging.info("config")
+    config = json.loads(r.execute_command('JSON.GET', message.chat.id))
+    logging.info(json.dumps(config))
+    if "winners_list" not in config:
+        config["winners_list"] = []
+    if winning_btc not in config["winners_list"]:
+        config["winners_list"] = {winning_btc: 1}
+    else:
+        config["winners_list"][winning_btc] = int(config["winners_list"][winning_btc]) + 1
+    if winning_eth not in config["winners_list"]:
+        config["winners_list"] = {winning_eth: 1}
+    else:
+        config["winners_list"][winning_eth] = int(config["winners_list"][winning_eth]) + 1
+    logging.info(json.dumps(config))
+    r.execute_command('JSON.SET', message.chat.id, '.', json.dumps(config))
+    await bot.send_message(chat_id=message.chat.id, text=f'Added To Table: ' + json.dumps(config["winners_list"]))
+    await bot.send_message(chat_id=message.chat.id, text='To clear all bets for this week, run /bets start')
+    
 
 @dp.message_handler(filters.RegexpCommandsFilter(regexp_commands=['bet btc ([0-9.,a-zA-Z]*) eth ([0-9.,a-zA-Z]*)']))
 async def set_weekly(message: types.Message, regexp_command):
@@ -169,6 +202,28 @@ async def set_weekly(message: types.Message, regexp_command):
         r.set("BTC_" + message.from_user.mention, amount)
         r.set("ETH_" + message.from_user.mention, amount_eth)
         await message.reply(f'Gotit. Bet for first Mars seat: BTC {amount}, ETH {amount_eth}')
+    except Exception as e:
+        await message.reply(f'{message.from_user.first_name} Fail. You Idiot. Try /bet btc 12.3k eth 1.2k')
+
+@dp.message_handler(filters.RegexpCommandsFilter(regexp_commands=['watch \$([a-zA-Z]*)']))
+async def add_to_prices(message: types.Message, regexp_command):
+    try:
+        new_coin = regexp_command.group(1)
+        logging.info("config")
+        config = json.loads(r.execute_command('JSON.GET', message.chat.id))
+        logging.info(json.dumps(config))
+        a, _, _ = get_price(new_coin)
+        if "watch_list" not in config:
+            config["watch_list"] = []
+        if a == 0:
+            await message.reply(f'{message.from_user.first_name} Fail. You Idiot. Code Not Found. Try /watch $aave')
+        else:
+            if new_coin in config["watch_list"]:
+                await message.reply(f'{message.from_user.first_name} Fail. Already Watching This One. ' + str(config["watch_list"]))
+            else:
+                config["watch_list"].append(new_coin)
+                r.execute_command('JSON.SET', message.chat.id, '.', json.dumps(config))
+                await message.reply(f'Gotit. Added ' + new_coin)
     except Exception as e:
         await message.reply(f'{message.from_user.first_name} Fail. You Idiot. Try /bet btc 12.3k eth 1.2k')
 
