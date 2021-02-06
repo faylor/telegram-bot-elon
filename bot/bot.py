e2519f6e042001796744d61d3abda9b6eb588ca1
faylor <32422243+faylor@users.noreply.github.com>
2021-02-06 13:15:38 +0000

    add me back in

--- bot/bot.py
+++ bot/bot.py
@@ -49,7 +49,7 @@ async def send_jelly(message: types.Message, regexp_command):
 async def send_green(message: types.Message, regexp_command):
     await bot.send_sticker(chat_id=message.chat.id, sticker="https://tenor.com/view/spacex-bitcoin-rd_btc-elon-musk-elon-gif-20158067")
 
-@dp.message_handler(commands=['elon', 'Elon', 'elon?', 'Elon?', 'help'])
+@dp.message_handler(commands=['elon', 'Elon', 'elon?', 'Elon?', 'help', 'me'])
 async def send_help(message: types.Message):
     await message.reply(f'SUP! {message.from_user.first_name}? \n Get Price: /$btc /$aave ..etc \n\n Watch Table: /$ /lambo /prices \n Add Coin To Watch: /watch <coin> eg: /watch eth \n Mark A Point: /buy btc \n Remove a Point: /sell btc \n\n bets: \n Start a new week: /startbets \n Finish a week: /stopbets \n Add your bet: /bet btc 12.3k eth 1.2k \n View current bets: /bets \n View Winners: /totes or /leaderboard \n\n Fun:\n /jelly <name>  \n /green<anything> \n /red<anything> \n /doge \n /cat \n /remind')
 
