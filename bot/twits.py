9bc60e7fec998dc51fcbc52ecf3a3ae734872ed2
faylor <32422243+faylor@users.noreply.github.com>
2021-02-08 14:38:41 +0000

    add bit

--- bot/twits.py
+++ bot/twits.py
@@ -72,7 +72,7 @@ class Twits:
         # You can adjust the rules if needed
         sample_rules = [
             {"value": "doge from:elonmusk", "tag": "doge 5dog12"},
-            {"value": "cat from:5dog12", "tag": "btc 5dog12"},
+            {"value": "bitcoin from:elonmusk", "tag": "btc 5dog12"},
         ]
         payload = {"add": sample_rules}
         response = requests.post(self.twitter_stream_url + "/rules", headers=self.headers, json=payload)
