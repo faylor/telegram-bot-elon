3593a6d19d2f287b2a736cdd91d764221ab1a85e
faylor <32422243+faylor@users.noreply.github.com>
2021-02-11 22:37:53 +0000

    backoff factor 2

--- bot/prices.py
+++ bot/prices.py
@@ -5,9 +5,10 @@ from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
 retry_strategy = Retry(
-    total=5,
+    total=3,
     status_forcelist=[429, 500, 502, 503, 504],
-    method_whitelist=["HEAD", "GET", "OPTIONS"]
+    method_whitelist=["HEAD", "GET", "OPTIONS"],
+    backoff_factor=2
 )
 adapter = HTTPAdapter(max_retries=retry_strategy)
 http = requests.Session()
