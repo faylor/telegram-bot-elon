db4310e62530466bfbef582dc4e1755dd4d3d545
faylor <32422243+faylor@users.noreply.github.com>
2021-02-18 12:46:33 +0000

    Reduce timeouts and retries

--- bot/prices.py
+++ bot/prices.py
@@ -6,10 +6,10 @@ from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
 retry_strategy = Retry(
-    total=3,
+    total=1,
     status_forcelist=[429, 500, 502, 503, 504],
     method_whitelist=["HEAD", "GET", "OPTIONS"],
-    backoff_factor=2
+    backoff_factor=1
 )
 adapter = HTTPAdapter(max_retries=retry_strategy)
 http = requests.Session()
