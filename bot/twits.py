12a3799001bab6ace1eb6dd612ba3b716d9c0a84
faylor <32422243+faylor@users.noreply.github.com>
2021-02-19 13:03:45 +0000

    add more

--- bot/twits.py
+++ bot/twits.py
@@ -101,7 +101,9 @@ class Twits:
     def set_stream_rules(self):
         # You can adjust the rules if needed
         sample_rules = [
-            {"value": "doge from:elonmusk", "tag": "doge 5dog12"}
+            {"value": "from:elonmusk", "tag": "doge 5dog12"},
+            {"value": "from:tyler", "tag": "ty 5dog12"},
+            {"value": "from:cameron", "tag": "cam 5dog12"}
         ]
         payload = {"add": sample_rules}
         response = requests.post(self.twitter_stream_url + "/rules", headers=self.headers, json=payload)
