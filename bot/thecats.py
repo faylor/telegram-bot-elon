22e9f9ae71e1ad6f5f36e838f2b79ccddd700bc8
faylor <32422243+faylor@users.noreply.github.com>
2021-02-20 10:12:11 +0000

    fix random

--- bot/thecats.py
+++ bot/thecats.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import urllib.parse
+from random import *
 
 def getTheApiUrl(animal):
     contents = requests.get('https://api.the' + animal + 'api.com/v1/images/search')
@@ -24,12 +25,12 @@ def search_pix(query):
     hits = js["hits"]
     url = None 
     if len(hits) > 0:
-        first_hit = hits[0]
-        print(first_hit)
-        if "previewURL" in first_hit:
-            url = first_hit["previewURL"]
-        elif "webformatURL" in first_hit:
+        index = randint(0, len(hits) - 1)
+        first_hit = hits[index]
+        if "webformatURL" in first_hit:
             url = first_hit["webformatURL"]
+        elif "previewURL" in first_hit:
+            url = first_hit["previewURL"]
     return url
 	
     
