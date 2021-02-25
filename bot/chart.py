747b225a4fff94b498aab3569165bf32e243db44
faylor <32422243+faylor@users.noreply.github.com>
2021-02-25 23:54:39 +0000

    Try numpy

--- bot/chart.py
+++ bot/chart.py
@@ -5,7 +5,7 @@ import redis
 import asyncio
 import pandas as pd
 import mplfinance as mpf
- 
+import numpy as np
 from aiogram import Bot, types
 from aiogram.types.input_file import InputFile
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
@@ -48,8 +48,11 @@ async def candle(message: types.Message):
     try:
         # trades = get_ohcl_trades('btc')
         arr = [[1614232980,41547.93,41572.28,41516.31,41532.75,1.062377,44130.68208997],[1614233040,41530.75,41532.75,41471.28,41474.81,0.74467,30908.75414289],[1614233100,41477.08,41537.03,41475.2,41500.39,0.864615,35878.76945827]]
+        arr = np.array(arr)
         df = pd.DataFrame(arr, columns='time open high low close volume amount'.split())
         df['time'] = pd.DatetimeIndex(df['time']*10**9)
+
+        logging.error("HERE")
         mpf.plot(df, type='candle', style='charles',
             title='S&P 500, Nov 2019',
             ylabel='Price ($)',
