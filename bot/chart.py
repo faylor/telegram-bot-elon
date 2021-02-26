9fd60bafd513dbebfac647dc7064a9d0abcc7260
faylor <32422243+faylor@users.noreply.github.com>
2021-02-26 09:37:10 +0000

    Update chart

--- bot/chart.py
+++ bot/chart.py
@@ -46,19 +46,31 @@ async def candle(message: types.Message, regexp_command):
     try:
         coin = regexp_command.group(1)
         trades = get_ohcl_trades(coin)
-        trades = trades[:60]
+        trades = trades[:35]
         df = pd.DataFrame(trades, columns='time open high low close volume amount'.split())
         df['time'] = pd.DatetimeIndex(df['time']*10**9)
         df.set_index('time', inplace=True)
 
-        mpf.plot(df, type='candle', style='yahoo',
+        df['MA20'] = df['close'].rolling(window=20).mean()
+        df['20dSTD'] = df['close'].rolling(window=20).std() 
+
+        df['Upper'] = df['MA20'] + (df['20dSTD'] * 2)
+        df['Lower'] = df['MA20'] - (df['20dSTD'] * 2)
+
+        df = df.tail(35)
+
+        apd  = [mpf.make_addplot(df['Lower'],color='#EC407A',width=0.9),
+                mpf.make_addplot(df['Upper'],color='#42A5F5', width=0.9),
+            mpf.make_addplot(df['MA20'],color='#FFEB3B',width=0.9)]
+
+        kwargs = dict(type='candle',
             title=coin.upper() + " vs USDT",
             ylabel='Price ($)',
-            ylabel_lower='Vol',
-            volume=True, 
-            mav=(3,6,9),
-            figscale=3.0,
-            savefig=coin + '-mplfiance.png')
+            ylabel_lower='Vol',volume=True,figratio=(3,2),figscale=2.1,addplot=apd)
+        mpf.plot(df,**kwargs,style='nightclouds')
+        mc = mpf.make_marketcolors(up='#00E676',down='#FF3D00',inherit=True)
+        s  = mpf.make_mpf_style(base_mpf_style='nightclouds',facecolor='#121212',edgecolor="#131313",gridcolor="#232323",marketcolors=mc)
+        mpf.plot(df,**kwargs, style=s, savefig=coin + '-mplfiance.png')
         
         await bot.send_photo(chat_id=chat_id, photo=InputFile(coin + '-mplfiance.png'))
     except Exception as e:
