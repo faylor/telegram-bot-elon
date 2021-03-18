80a8b8b0374d931034e7a721aa17aaf17ed36f02
faylor <32422243+faylor@users.noreply.github.com>
2021-03-18 23:02:50 +0000

    try with name

--- bot/streamprices.py
+++ bot/streamprices.py
@@ -12,24 +12,23 @@ class Crytream():
         self.bot = None
 
     
-    def handle_intervals_update(self, interval_update):
+    async def handle_intervals_update(self, intervals_update):
         # market_msg = ">>> Market#{} Exchange#{} Pair#{}: {} New Trades".format(
         #     trade_update.marketUpdate.market.marketId,
         #     trade_update.marketUpdate.market.exchangeId,
         #     trade_update.marketUpdate.market.currencyPairId,
         #     len(trade_update.marketUpdate.tradesUpdate.trades),
         # )
-        print(interval_update)
-        # for interval in intervals_update.marketUpdate.intervalsUpdate.intervals:
-        # if self.bot is not None:
-        #     await self.bot.send_message(chat_id=self.chat_id[0], text=str(interval))
-        #     # if interval.period == 60:
-                # trade_msg = "Base:{} Quote:{}".format(
-                #     interval.volumeBaseStr,
-                #     interval.volumeQuoteStr
-                # )
-            
-                # print(trade_msg)
+        # print(market_msg)
+        for interval in intervals_update.marketUpdate.intervalsUpdate.intervals:
+            if interval.periodName == "60":
+                trade_msg = "\tBase:{} Quote:{}".format(
+                    interval.volumeBaseStr,
+                    interval.volumeQuoteStr
+                )
+                if self.bot is not None:
+                    await self.bot.send_message(chat_id=self.chat_id[0], text=trade_msg)
+                print(trade_msg)
     
     def add_chat_id(self, chat_id):
         if chat_id not in self.chat_ids:
