7a0554fd5f366e9b0662a9c599dddc495002b39c
faylor <32422243+faylor@users.noreply.github.com>
2021-03-19 09:03:03 +0000

    add detector for volume

--- bot/streamprices.py
+++ bot/streamprices.py
@@ -16,6 +16,7 @@ class Crytream():
         cw.stream.subscriptions = ["assets:60:ohlc"]
         # cw.stream.on_trades_update = self.handle_trades_update
         self.bot = None
+        self.volumes = []
 
     
 
@@ -27,13 +28,46 @@ class Crytream():
         #     len(trade_update.marketUpdate.tradesUpdate.trades),
         # )
         # print(market_msg)
-        text = MessageToJson(interval_update)
-        # for interval in interval_update.marketUpdate.intervalsUpdate.intervals:
-        #     if interval.periodName == "60":
-        bot_key = TELEGRAM_BOT
-        chat_id = self.chat_ids[0]
-        send_message_url = f'https://api.telegram.org/bot{bot_key}/sendMessage?chat_id={chat_id}&text={text}'
-        resp = requests.post(send_message_url)
+        #         {
+        #   "marketUpdate": {
+        #     "intervalsUpdate": {
+        #       "intervals": [
+        #         {
+        #           "closetime": "1616142540",
+        #           "ohlc": {
+        #             "openStr": "54121.8",
+        #             "highStr": "54161",
+        #             "lowStr": "54121.6",
+        #             "closeStr": "54160.5"
+        #           },
+        #           "volumeBaseStr": "0.11336775",
+        #           "volumeQuoteStr": "6139.861519969",
+        #           "periodName": "60"
+        #         }
+        #       ]
+        #     },
+        #     "market": {
+        #       "exchangeId": "4",
+        #       "currencyPairId": "5284",
+        #       "marketId": "61496"
+        #     }
+        #   }
+        # }
+        last_message = MessageToJson(interval_update)
+        intervals = last_message["marketUpdate"]["intervalsUpdate"]["intervals"]
+        if len(self.volumes) > 5:
+            last_average = sum(self.volumes)/len(self.volumes)
+        for interval in intervals:
+            if interval["periodName"] == "60":
+                last_volume = float(interval["volumeQuoteStr"])
+                self.volumes.append(last_volume)
+        if len(self.volumes) > 5:
+            if last_average < last_volume * 5:
+                bot_key = TELEGRAM_BOT
+                chat_id = self.chat_ids[0]
+                text = "ALERT SPIKE IN BTC VOLUME: " + str(last_volume) + "  AVERAGE:" + str(last_average)
+                send_message_url = f'https://api.telegram.org/bot{bot_key}/sendMessage?chat_id={chat_id}&text={text}'
+                resp = requests.post(send_message_url)
     
     
     def add_chat_id(self, chat_id):
