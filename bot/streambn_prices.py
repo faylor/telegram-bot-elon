bb4924f2b12ee8634db2eb563dd57097cc78ab6f
faylor <32422243+faylor@users.noreply.github.com>
2021-03-29 14:12:53 +0100

    try checking vol

--- bot/streambn_prices.py
+++ bot/streambn_prices.py
@@ -1,12 +1,17 @@
 import os
 import json
 import asyncio
-
+import logging
 import requests
 from bot.settings import (BN_API_KEY, BN_API_SECRET, TELEGRAM_BOT)
 from binance.websockets import BinanceSocketManager
 from binance.client import Client
 from binance.enums import *
+import redis
+from .settings import (REDIS_URL)
+r = redis.from_url(REDIS_URL)
+
+COIN_DATA_KEY = "STREAM_{}"
 
 class Bntream():
 
@@ -22,13 +27,35 @@ class Bntream():
         self.conn_key = ""
            
     def process_message(self, msg):
-        print("message type: {}".format(msg['e']))
-        print(msg)
-        if len(self.chat_ids) > 0:
-            chat_id = self.chat_ids[0]
-            text = "BN Message: " + json.dumps(msg)
-            send_message_url = f'https://api.telegram.org/bot{TELEGRAM_BOT}/sendMessage?chat_id={chat_id}&text={text}'
-            resp = requests.post(send_message_url)
+        try:
+            taker_buy_vol = msg["k"]["Q"]
+            data = r.get(COIN_DATA_KEY.format("AUDIO"))
+            if data is not None:
+                js = json.loads(data.decode("utf-8"))
+                if "Q" in js:
+                    if js["Q"] is None:
+                        js = {"Q": [taker_buy_vol]}
+                    else:
+                        if len(js["Q"]) > 3:
+                            last_VOL = js["Q"][-1]
+                            diff = 100 * (taker_buy_vol - last_VOL)/last_VOL
+                            if abs(diff) > 1:
+                                bot_key = TELEGRAM_BOT
+                                chat_id = self.chat_ids[0]
+                                text = "AUDIO RAPID CHANGE " + str(round(float(diff),1)) + "%"
+                                send_message_url = f'https://api.telegram.org/bot{bot_key}/sendMessage?chat_id={chat_id}&text={text}'
+                                resp = requests.post(send_message_url)
+                        if len(js["Q"]) > 50:
+                            js["Q"] = js["Q"][:25]
+                        js["Q"].append(taker_buy_vol) 
+                else:
+                    js = {"Q": [taker_buy_vol]}
+            else:
+                js = {"Q": [taker_buy_vol]}
+            r.set(COIN_DATA_KEY.format("AUDIO"), json.dumps(js))
+            self.stored = self.stored + 1
+        except Exception as e:
+            logging.error("BN Stream process error:" + str(e))
         # do something
 
     # def handle_intervals_update(self, interval_update):
