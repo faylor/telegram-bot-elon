a498dc1241a2fe32bf0700b7c051cd45cf895756
faylor <32422243+faylor@users.noreply.github.com>
2021-03-31 15:57:52 +0100

    remove info

--- bot/bn_testorders.py
+++ bot/bn_testorders.py
@@ -39,14 +39,12 @@ class BnOrder():
 
     def get_wallet(self):
         try:
-            logging.error("Account settings here:" + str(self.chat_ids[0]))
-            info = self.client.get_account()
-            logging.error("Account settings here:" + str(info))
+            # info = self.client.get_account()
             balance = self.client.get_asset_balance(asset='BNB')
             trades = self.client.get_my_trades(symbol='BNBBTC')
             bot_key = TELEGRAM_BOT
             chat_id = self.chat_ids[0]
-            text = "ACCOUNT INFO: " + json.dumps(info) + "\nBALANCE:" + balance + "\nTRADES:" + trades
+            text = "ACCOUNT BNB BALANCE:" + str(balance) + "\nTRADES:" + json.dumps(trades)
             send_message_url = f'https://api.telegram.org/bot{bot_key}/sendMessage?chat_id={chat_id}&text={text}'
             resp = requests.post(send_message_url)
         except Exception as e:
