39aa098710ddfee6270d214f0747bd4f33d7e8e0
faylor <32422243+faylor@users.noreply.github.com>
2021-03-31 14:53:36 +0100

    better tracker

--- bot/streambn_prices.py
+++ bot/streambn_prices.py
@@ -26,16 +26,19 @@ class Bntream():
         self.sell_updates = 0
         self.buy_updates = 0
         self.conn_key = ""
+        self.conn_mt_key = ""
         self.green_count = {}
         self.red_count = {}
         self.velocity = {}
         self.velocity_previous = {}
+        self.miniticker = {}
            
     def process_message(self, msg):
         try:
             stream, data = msg['stream'], msg['data']
             taker_buy_vol = float(data["k"]["Q"])
             is_end = data["k"]["x"]
+            symbol = stream.lower().replace("usdt@kline_1m", "")
             if is_end == True:
                 open_price = float(data["k"]["o"])
                 close_price = float(data["k"]["c"])
@@ -87,18 +90,37 @@ class Bntream():
                 r.set(COIN_DATA_KEY.format("AUDIO"), json.dumps(js))
                 self.stored = self.stored + 1
             elif stream in self.green_count and self.green_count[stream] > 1 and self.velocity[stream]  > self.velocity_previous[stream]:
-                bot_key = TELEGRAM_BOT
-                chat_id = self.chat_ids[0]
-                ratio = self.velocity[stream]/self.velocity_previous[stream]
-                text = stream.replace("usdt@kline_1m", "").upper() + " " + str(self.green_count[stream]) + " GREEN. Velocity:" + str(round(ratio,1)) + "  Buy Vol:" + str(round(float(taker_buy_vol),1)) 
-                send_message_url = f'https://api.telegram.org/bot{bot_key}/sendMessage?chat_id={chat_id}&text={text}'
-                resp = requests.post(send_message_url)
-                self.green_count[stream] = 0
+                if symbol in self.miniticker:
+                    high_to_low_diff = float(self.miniticker[symbol]["h"]) - float(self.miniticker[symbol]["l"])
+                    high_to_price_diff = float(self.miniticker[symbol]["h"]) - float(data["k"]["c"])
+                    low_to_price_diff = float(data["k"]["c"]) - float(self.miniticker[symbol]["l"])
+                    bot_key = TELEGRAM_BOT
+                    chat_id = self.chat_ids[0]
+                    ratio = self.velocity[stream]/self.velocity_previous[stream]
+                    text = f"""{symbol} {self.green_count[stream]}  GREEN\n 
+                            24hr High - 24hr Low: {round(high_to_low_diff, 4)}\n
+                            24hr High - Price: {round(high_to_price_diff, 4)}\n
+                            Price - 24hr Low: {round(low_to_price_diff, 4)}\n
+                            Velocity: {round(ratio,1)}\n
+                            Buy Vol: {round(float(taker_buy_vol),1)}
+                    """
+                    send_message_url = f'https://api.telegram.org/bot{bot_key}/sendMessage?chat_id={chat_id}&text={text}'
+                    resp = requests.post(send_message_url)
+                    self.green_count[stream] = 0
             elif stream in self.red_count and self.red_count[stream] > 1 and self.velocity[stream] < self.velocity_previous[stream]:
+                high_to_low_diff = float(self.miniticker[symbol]["h"]) - float(self.miniticker[symbol]["l"])
+                high_to_price_diff = float(self.miniticker[symbol]["h"]) - float(data["k"]["c"])
+                low_to_price_diff = float(data["k"]["c"]) - float(self.miniticker[symbol]["l"])
                 bot_key = TELEGRAM_BOT
                 chat_id = self.chat_ids[0]
                 ratio = self.velocity[stream]/self.velocity_previous[stream]
-                text = stream.replace("usdt@kline_1m", "").upper() + " " + str(self.red_count[stream]) + " RED. Velocity:" + str(round(ratio,1)) + "  Buy Vol:" + str(round(float(taker_buy_vol),1)) 
+                text = f"""{symbol} {self.green_count[stream]}  RED\n 
+                        24hr High - 24hr Low: {round(high_to_low_diff, 4)}\n
+                        24hr High - Price: {round(high_to_price_diff, 4)}\n
+                        Price - 24hr Low: {round(low_to_price_diff, 4)}\n
+                        Velocity: {round(ratio,1)}\n
+                        Buy Vol: {round(float(taker_buy_vol),1)}
+                """
                 send_message_url = f'https://api.telegram.org/bot{bot_key}/sendMessage?chat_id={chat_id}&text={text}'
                 resp = requests.post(send_message_url)
                 self.red_count[stream] = 0
@@ -106,8 +128,24 @@ class Bntream():
             logging.error("BN Stream process error:" + str(e))
         # do something
 
-    # def handle_intervals_update(self, interval_update):
-    #     klines = client.get_historical_klines("BNBBTC", Client.KLINE_INTERVAL_1MINUTE, "1 day ago UTC")
+    def process_miniticker(self, msg):
+        try:
+            stream, data = msg['stream'], msg['data']
+            #    {
+            #     "e": "24hrMiniTicker",  // Event type
+            #     "E": 123456789,         // Event time
+            #     "s": "BNBBTC",          // Symbol
+            #     "c": "0.0025",          // Close price
+            #     "o": "0.0010",          // Open price
+            #     "h": "0.0025",          // High price
+            #     "l": "0.0010",          // Low price
+            #     "v": "10000",           // Total traded base asset volume
+            #     "q": "18"               // Total traded quote asset volume
+            #   }
+            symbol = data["s"].lower().replace("usdt", "")
+            self.miniticker[symbol] = data
+        except Exception as e:
+            logging.error("BN MT Stream process error:" + str(e))
 
    
     def add_chat_id(self, chat_id):
@@ -119,10 +157,12 @@ class Bntream():
             self.chat_ids.remove(chat_id)
 
     def start(self):
-        self.conn_key = self.bm.start_multiplex_socket(['audiousdt@kline_1m', 'btcusdt@kline_1m'], self.process_message)
+        self.conn_key = self.bm.start_multiplex_socket(['audiousdt@kline_1m', 'bnbusdt@kline_1m', 'grtusdt@kline_1m',  'btcusdt@kline_1m'], self.process_message)
+        self.conn_mt_key = self.bm.start_multiplex_socket(['audiousdt@miniTicker', 'bnbusdt@miniTicker', 'grtusdt@miniTicker', 'btcusdt@miniTicker'], self.process_miniticker)
         # self.conn_key = self.bm.start_kline_socket('AUDIOUSDT', self.process_message, interval=KLINE_INTERVAL_1MINUTE)
         # self.conn_key = self.bm.start_aggtrade_socket('BNBBTC', self.process_message)
         self.bm.start()
 
     def stop(self):
         self.bm.stop_socket(self.conn_key)
+        self.bm.stop_socket(self.conn_mt_key)
