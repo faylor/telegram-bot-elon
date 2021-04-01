1e64af22ae8cc90c54fa1a0cc2183cb6fdf4678a
faylor <32422243+faylor@users.noreply.github.com>
2021-04-01 14:21:57 +0100

    try account

--- bot/bot.py
+++ bot/bot.py
@@ -161,7 +161,7 @@ async def bn_order_start(message: types.Message, regexp_command):
         logging.error("START UP ERROR:" + str(e))
         await bot.send_message(chat_id=message.chat.id, text="Failed to Start Stream")
 
-@dp.message_handler(commands=['balance'])
+@dp.message_handler(commands=['account'])
 async def get_balance(message: types.Message):
     try:
         bn_order.get_wallet(message.chat.id)
