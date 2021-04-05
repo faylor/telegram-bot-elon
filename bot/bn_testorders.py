77949e24109c55deba9f0abace357e3530459690
faylor <32422243+faylor@users.noreply.github.com>
2021-04-05 22:40:03 +0100

    move to account

--- bot/bn_testorders.py
+++ bot/bn_testorders.py
@@ -21,8 +21,9 @@ class BnOrder():
     def __init__(self) -> None:
         self.chat_id = BN_CHAT_ID 
         self.chat_id = BN_CHAT_ID_GROUP
-        self.client = Client(BN_TEST_API_KEY, BN_TEST_API_SECRET)
-        self.client.API_URL = 'https://testnet.binance.vision/api'
+        # self.client = Client(BN_TEST_API_KEY, BN_TEST_API_SECRET)
+        # self.client.API_URL = 'https://testnet.binance.vision/api'
+        self.client = Client(BN_API_KEY, BN_API_SECRET)
         self.bm = BinanceSocketManager(self.client)        
         
     def process_message(self, msg):
