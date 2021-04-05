4fd94359de2b639a9290b765d186f9e1d1a358d2
faylor <32422243+faylor@users.noreply.github.com>
2021-04-05 13:10:33 +0100

    add log

--- bot/bn_testorders.py
+++ bot/bn_testorders.py
@@ -135,7 +135,8 @@ class BnOrder():
                         stopLimitTimeInForce='GTC')
                 
                 oco_text = "OCO ORDERS:\n"
-                for o in json.loads(order_oco):
+                for o in order_oco:
+                    logging.error("HAHDHA" + json.dumps(o))
                     if o["type"] == "STOP_LOSS_LIMIT":
                         oco_text = oco_text + "STOP LOSS" + o["side"]  + ", Stop Limit: " + o["stopPrice"] + " Price: " + o["price"] + " Qty:" + o["origQty"] + "\n"
                     elif o["type"] == "LIMIT_MAKER":
