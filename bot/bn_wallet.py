e23eb9d768e1f0120b63b2c771c50c215d878e88
faylor <32422243+faylor@users.noreply.github.com>
2021-04-09 08:49:29 +0100

    add tsl

--- bot/bn_wallet.py
+++ bot/bn_wallet.py
@@ -73,6 +73,25 @@ async def bn_order_start(message: types.Message, regexp_command):
         logging.error("START UP ERROR:" + str(e))
         await bot.send_message(chat_id=message.chat.id, text="Failed to Start Stream")
 
+@dp.message_handler(filters.RegexpCommandsFilter(regexp_commands=['tsl ([\s0-9.,a-zA-Z]*)']))
+async def bn_tsl_start(message: types.Message, regexp_command):
+    try:
+        all = regexp_command.group(1)
+        buy_or_sell, first_coin, second_coin = all.strip().upper().split()
+        if buy_or_sell not in ["BUY", "SELL"]:
+            return await bot.send_message(chat_id=message.chat.id, text="Failed to Market Buy or Sell, must enter buy or sell first and then Coin given, eg: /market buy eth")
+        if buy_or_sell == "BUY":
+            buying_coin = first_coin
+            selling_coin = second_coin
+        else:
+            buying_coin = second_coin
+            selling_coin = first_coin
+        bn_order.create_trailing_stop_limit(market=buying_coin + selling_coin, buy_coin=buying_coin, sell_coin=selling_coin, type=buy_or_sell.lower(), stop_percentage=0.099, interval=10)
+        
+    except Exception as e:
+        logging.error("START UP TSL ERROR:" + str(e))
+        await bot.send_message(chat_id=message.chat.id, text="Failed to Start Stream")
+
 
 @dp.message_handler(filters.RegexpCommandsFilter(regexp_commands=['market ([\s0-9.,a-zA-Z]*)']))
 async def bn_order_market_buy(message: types.Message, regexp_command, state: FSMContext):
@@ -200,7 +219,7 @@ async def process_spend(message: types.Message, state: FSMContext):
             elif data["tsl"] == True:
                 limit_type = "sell" if sale_type.upper() == "BUY" else "buy"
                 await message.reply("Creating Trailing Stop Loss...", reply_markup=markup)
-                bn_order.create_trailing_stop_limit(market=symbol, buy_coin=buy_coin, sell_coin=sell_coin, type=limit_type, stop_percentage=0.0192, interval=10)
+                bn_order.create_trailing_stop_limit(market=symbol, buy_coin=buy_coin, sell_coin=sell_coin, type=limit_type, stop_percentage=0.099, interval=10)
                 
             bn_order.get_wallet(message.chat.id)
             
