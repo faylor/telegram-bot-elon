8000c680593e2cbad8bd32e5ff049ae9232e8a86
faylor <32422243+faylor@users.noreply.github.com>
2021-05-24 09:27:14 +0100

    fix shopping list

--- bot/shoppinglist.py
+++ bot/shoppinglist.py
@@ -2,8 +2,14 @@ import logging
 import json
 from aiogram import types
 from aiogram.dispatcher import filters
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
 
 from .bot import dp, r, bot
+
+class ShopForm(StatesGroup):
+    item = State()
+
 
 @dp.message_handler(commands=['pickup'])
 async def pickup_list(message: types.Message):
@@ -76,3 +82,50 @@ async def remove_from_shop(message: types.Message, regexp_command):
         logging.warn(str(e))
         await message.reply(f'{message.from_user.first_name} Fail. You Idiot. ')
 
+@dp.message_handler(commands=['shop'])
+async def remove_from_shop_list(message: types.Message,  state: FSMContext):
+    try:
+        config = r.get(message.chat.id)
+        if config is not None:
+            config = json.loads(config)
+            if "shop_list" in config:
+                await ShopForm.item.set()
+                
+                markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+                markup.add(*config["shop_list"])
+                markup.add("Cancel")
+                    
+                await message.reply(f"Buying:\n", reply_markup=markup)
+   
+        await message.reply(f'{message.from_user.first_name} No shopping found, try /+bananas etc..')
+    except Exception as e:
+        logging.warn(str(e))
+        await message.reply(f'{message.from_user.first_name} Fail. You Idiot. ')
+
+
+@dp.message_handler(lambda message: message.text in ["cancel", "Cancel"], state=ShopForm.item)
+async def cancel_spent(message: types.Message, state: FSMContext):
+    await state.finish()
+    return await message.reply("Cancelled Shopping.")
+
+@dp.message_handler(state=ShopForm.item)
+async def process_shop_list(message: types.Message, state: FSMContext):
+    try:
+        async with state.proxy() as data:
+            markup = types.ReplyKeyboardRemove()
+            item_bought_response = message.text.lower().strip()
+            config = r.get(message.chat.id)
+            if config is not None:
+                config = json.loads(config)
+                if "shop_list" in config:
+                    if item_bought_response in config["shop_list"]:
+                        config["shop_list"].remove(item_bought_response)
+                        r.set(message.chat.id, json.dumps(config))
+                        await message.reply(f'{message.from_user.first_name}, done. You bought ' + str(item_bought_response))
+                    else:
+                        await message.reply(f'{message.from_user.first_name}, not found.. ' + str(item_bought_response))
+        # Finish conversation
+        await state.finish()
+    except Exception as e:
+        logging.error("SHOP ERROR:" + str(e))
+        await message.reply(f'{message.from_user.first_name} Fail. ' + str(e))
