c135df67a77a55d3803a86c6e2f08fc0953f4afe
faylor <32422243+faylor@users.noreply.github.com>
2021-08-06 10:19:22 +0100

    update to 4th

--- bot/virtualwallet.py
+++ bot/virtualwallet.py
@@ -734,7 +734,7 @@ async def totals_user_scores2(message: types.Message):
                     scores.append(score_total)
                     score_live = str(round_sense(score_live)).ljust(10, ' ')
                     out.append(f"🔸 {user} {score_live} {score_usd_str}")
-        out.append("</pre>\nEnds Saturday 5th September, MAXIMUM TRADES (GRAB LOCKS) = " + str(MAX_TRADES))
+        out.append("</pre>\nEnds Saturday 4th September, MAXIMUM TRADES (GRAB LOCKS) = " + str(MAX_TRADES))
         if len(out) > 3:
             out[1] =  out[1].replace('🔸', '👑')
             out[len(out)-2] = out[len(out)-2].replace('🔸', '🥄')
