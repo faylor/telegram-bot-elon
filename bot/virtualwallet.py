b8c3ea0aad348391ccd369b3d4a73ba2837c6a14
faylor <32422243+faylor@users.noreply.github.com>
2021-08-06 13:02:12 +0100

    clear locks

--- bot/virtualwallet.py
+++ bot/virtualwallet.py
@@ -115,6 +115,20 @@ async def reset_cards(message: types.Message):
     except Exception as e:
         await message.reply(f'{message.from_user.first_name} Failed to clear cards.' + str(e))
 
+@dp.message_handler(commands=['clearlocks'])
+async def reset_locks(message: types.Message):
+    try:
+        chat_id = str(message.chat.id)
+        saves = r.scan_iter(SCORE_KEY.format(chat_id=chat_id, user_id="*"))
+        for key in saves:
+            key = key.decode('utf-8')
+            user_id = key.replace(chat_id + "_bagscore_", "")
+            saves = r.delete(TRADE_LOCK_KEY.format(chat_id=chat_id, user_id=user_id))
+        await message.reply(f'Ok cleared locks from this Chat.')
+    except Exception as e:
+        await message.reply(f'{message.from_user.first_name} Failed to clear locks.' + str(e))
+
+
 
 @dp.message_handler(commands=['clearthischatcards'])
 async def reset_cards(message: types.Message):
