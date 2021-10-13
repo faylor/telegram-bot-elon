cac5dad625902a89f0f93972f7a22bd76a00343c
faylor <32422243+faylor@users.noreply.github.com>
2021-10-13 21:36:31 +0100

    debug again

--- bot/prices.py
+++ bot/prices.py
@@ -149,6 +149,7 @@ def get_ath_ranks(labels):
         return None
 
     try:
+        logging.error("Unable to sort ATH data:" + json.dumps(results))
         marklist = sorted(results.items(), key=lambda x:x[1]['down_from_alt'])
         return dict(marklist)
     except Exception as e:
