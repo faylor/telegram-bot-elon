1bf74a5d0dec9c19f2da10268321500740221b56
faylor <32422243+faylor@users.noreply.github.com>
2021-10-16 13:54:54 +0100

    add message to draw_4 user

--- bot/virtualwallet.py
+++ bot/virtualwallet.py
@@ -307,6 +307,7 @@ async def use_card_to_user(message: types.Message, state: FSMContext):
                                 new_balance = await grab_for_user(chat_id=chat_id, user_id=to_user_id, coin=coin, balance=remaining_balance, ratio=ratio)
                                 if new_balance < remaining_balance:
                                     remaining_balance = new_balance
+                                    await message.reply(f"DRAW 4: Present from {message.from_user.mention} to {mention_name}. {coin}", reply_markup=markup)
                                     ii = ii + 1
                         if ok == 1:
                             ok = delete_card(message.from_user.id, data["chat_id"], "draw_4")
