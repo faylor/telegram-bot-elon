aa8c87c468057601265c2a7035df53d61daa8e9b
faylor <32422243+faylor@users.noreply.github.com>
2022-04-19 17:58:41 +0100

    done

---
+++ bot/scheduler.py
@@ -0,0 +1,17 @@
+# Scheduling persistence
+
+PERSISTENCE_KEY = "running_schedules"
+
+def get_schedules():
+    schedules = open PERSISTENCE_KEY
+    for each schedule in schedules:
+        is not empty:
+            is not running already:
+                has time run out?
+                    close item if close enough in time ???
+                else:
+                    start watcher again, assume restart.
+                    restart_schedule(schedule_id)
+
+
+def restart_scheduler(schedule_id):
