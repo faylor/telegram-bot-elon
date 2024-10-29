802b920d0d7fc6dfbb7aab41f6ecd95872a86aa7
faylor <32422243+faylor@users.noreply.github.com>
2024-10-29 12:30:19 +0000

    Testing Startup

--- do.py
+++ do.py
@@ -5,13 +5,18 @@ def set_hook():
     import asyncio
     from bot.settings import HEROKU_APP_NAME, WEBHOOK_URL, TELEGRAM_BOT
     from aiogram import Bot
+    
+    print('Setting Bot ' + TELEGRAM_BOT)
     bot = Bot(token=TELEGRAM_BOT)
+    print('Set Bot ' + TELEGRAM_BOT)
 
     async def hook_set():
         if not HEROKU_APP_NAME:
             print('You have forgot to set HEROKU_APP_NAME')
             quit()
+        print('Setting webhook ' + WEBHOOK_URL)
         await bot.set_webhook(WEBHOOK_URL)
+        print('Set webhook ' + WEBHOOK_URL)
         print(await bot.get_webhook_info())
     
 
